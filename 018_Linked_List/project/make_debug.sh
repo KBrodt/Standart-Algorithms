3f3e6007881ff2b54a5df34a6785ac4356aa39ac
Kirill Brodt <cyrill.brodt@gmail.com>
2016-08-18 23:35:06 +0700

    initializer_list was added

--- 018_Linked_List/project/make_debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../bin/linux_debug
-make compile_flags="-std=c++11 -Wall -O0 -g"     --directory=../bin/linux_debug     --makefile=../../project/Makefile
--- 018_Linked_List/project/make_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p ../bin/linux_release
-make compile_flags="-std=c++11 -O3 -funroll-loops -fomit-frame-pointer"     --directory=../bin/linux_release     --makefile=../../project/Makefile
